fab3623defd7b77393d42ff5551d6a10f1707b43
Abel Lucas <abelucas009@gmail.com>
2023-03-08 10:53:44 -0300

    Em qual mão esta? commit

---
+++ Em Qual Mão está.py
@@ -0,0 +1,66 @@
+###################
+#Em Qual Mão Está?#
+###################
+import pyxel
+
+class Hand(object):
+    def __init__(self, x=int, y=int, imgx=int, imgy=int, w=int, h=int):
+        self.x, self.y= x,y
+        self.w, self.h= w,h
+        self.imgx, self.imgy= imgx,imgy
+        self.ring= pyxel.rndi(0,1)
+
+    #Método de interação com as mãos
+    def verClick(self):
+        if pyxel.mouse_x >= self.x+4 and pyxel.mouse_x <= self.x +self.w-4:
+            if pyxel.mouse_y >= self.y and pyxel.mouse_y <= self.y +self.h-7:
+                self.imgx= 32       #Troca de sprite
+                if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT):
+                    if self.ring:
+                        self.imgx= 128  #Troca de sprite(com o anel)
+                    else:self.imgx= 96  #Troca de sprite(sem o anel)
+        else: self.imgx=0    #Troca de sprite
+
+    #Método de atualização da mão na interface
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, self.imgx, self.imgy, self.w, self.h)
+
+class Game:
+    def __init__(self):
+        pyxel.init(90,140,"Em Qual Mão está?")
+        #Declarando variaveis
+        self.play= False
+        self.rightHand= Hand(10,50,0,0,32,32)#13
+        self.leftHand= Hand(42,50,0,32,32,32)#45
+        self.handsList= [self.rightHand,self.leftHand]
+        pyxel.load("resources/Em_Qual_Mão_Está.pyxres")
+        pyxel.run(self.update, self.draw)
+
+    #Método de verificação de interação a cada quadro
+    def update(self):
+        #se o botão play for apertado:
+        if pyxel.btn(pyxel.MOUSE_BUTTON_LEFT): self.play=True
+
+        if self.play:
+            #Evitando que o anel surja nas 2 mãos
+            if self.rightHand.ring == 1: self.leftHand.ring= 0
+            if self.rightHand.ring== 0:  self.leftHand.ring= 1
+            #Verificando interação com os Objetos
+            for hand in self.handsList:
+                hand.verClick()
+        else: pass
+
+    #Método de atualização da interface
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.mouse(True)
+        if self.play:
+            #Iserindo/Desenhando Objetos na tela
+            for hand in self.handsList:
+                hand.draw()
+        else:
+            pyxel.blt(0,0,1,0,0,90,90)
+            pyxel.text(3,110,"Clique para continuar", pyxel.frame_count % 8)
+#Verificação de execução direta do módulo
+if __name__ == "__main__":
+    Game()
--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Em-Qual-Mão-Está?
- 
+<div align="center">
+<div>
+<img src="img/icon.png" alt="Em Qual Mão Está? icon">
+</div>
+
+<div>
+  <h4>🚧⚠️Em desenvolvimento...⚠️🚧</h4>
+</div>
+
+<div>
+  <h3>Sobre o projeto</h3>
+  <p>Criado através da linguagem python, com objetivo de começar um projeto do mais simples possível, o "Em Qual Mão Está?" é um game com jogabilidade simples que utiliza a interface gráfica e cálculos matemáticos do módulo pyxel para brincar com valores aleatórios.</p>
+  <p>
+    <a href="https://www.python.org/">
+      <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" alt="Python V.3.9" ></a>
+    <a href="https://github.com/kitao/pyxel">
+      <img src="https://img.shields.io/badge/Pyxel-v1.9.12-blue?style=for-the-badge&logo=python&logoColor=white" alt="Pyxel V.1.9.12" ></a>
+  </p>
+
+<div>
+  <h3>Resultado</h3>
+  <div>
+    <img src="img/interface.png" alt="Interface do Em Qual Mão Está?" width=300px>
+    <img src="img/Em Qual Mão Está.gif" alt="Em Qual Mão Está? gif" width=317px>
+  </div>
+
+</div>
+</div>
+</div>
